2aa09a6f2d149fa90e9616ef4ac14fd53c86ac5b
Nekk <timeresdios@gmail.com>
2025-01-17 16:06:46 +0100

    Cambios en componente Boton.

--- app/ui/Boton.tsx
+++ app/ui/Boton.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from "react";
 
-export default function Boton({ buttonText, buttonIcon }: { 
+export default function Boton({ buttonText, buttonIcon, onClick}: { 
     buttonText: string; 
-    buttonIcon: ReactNode; 
+    buttonIcon: ReactNode;
+    onClick: () => void; 
 }) {
     return (
-        <button
+        <button onClick = {onClick}
             className="group flex items-center justify-center rounded-full w-10 h-10 bg-onyx-400 text-white transition-all duration-300 hover:w-40"
         >
             <span className="w-5 h-5">{buttonIcon}</span>
